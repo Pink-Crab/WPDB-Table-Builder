ef9944869c6e4be73a011920558ba6544f669c78
Glynn Quelch <glynn.quelch@gmail.com>
2022-03-20 00:25:57 +0000

    House Keeping

--- tests/Exception/Test_WPDB_Validator_Exception.php
+++ tests/Exception/Test_WPDB_Validator_Exception.php
@@ -15,21 +15,9 @@ namespace PinkCrab\Table_Builder\Tests\Exception;
 
 use WP_UnitTestCase;
 use PinkCrab\Table_Builder\Schema;
-use PinkCrab\Table_Builder\Exception\Engine_Exception;
-use PinkCrab\Table_Builder\Engines\WPDB_DB_Delta\DB_Delta_Engine;
 use PinkCrab\Table_Builder\Exception\WPDB_DB_Delta\WPDB_Validator_Exception;
 
 class Test_WPDB_Validator_Exception extends WP_UnitTestCase {
-
-	private $backup_wpdb;
-
-	public function setUp(): void {
-		$this->backup_wpdb = clone $GLOBALS['wpdb'];
-	}
-
-	public function tearDown(): void {
-		$GLOBALS['wpdb'] = clone $this->backup_wpdb;
-	}
 
 	/**
 	 * Returns a basic schema.
@@ -46,31 +34,6 @@ class Test_WPDB_Validator_Exception extends WP_UnitTestCase {
 				$schema->index( 'id' )->primary();
 			}
 		);
-	}
-
-
-
-	/** @testdox When drop a table, if an error is printed, throw exception. */
-	public function test_throw_exception_drop_when_prints_error() {
-
-		$GLOBALS['wpdb'] = $this->createMock( \wpdb::class );
-		// Mock methods used for db_delta.
-		$GLOBALS['wpdb']->method( 'get_results' )
-			->will(
-				$this->returnCallback(
-					function( $query ) {
-						echo 'Printed error';
-					}
-				)
-			);
-
-		$engine = new DB_Delta_Engine( $GLOBALS['wpdb'] );
-		$schema = $this->get_schema();
-
-		$this->expectException( Engine_Exception::class );
-		$this->expectExceptionCode( 102 );
-		$this->expectExceptionMessage( 'Printed error' );
-		$engine->drop_table( $schema );
 	}
 
 	/** @testdox It should be possible to access the schema that generated the exception. */
